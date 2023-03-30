85045309f6c835c2891c3b466e4124a607930b78
robertkottelin <robert.kottelin@gmail.com>
2023-03-30 21:59:04 +0200

    added user handling

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,50 @@
 use tokio::{
     io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
     net::TcpListener,
+    net::TcpStream,
     sync::broadcast,
 };
+use std::{collections::HashMap, net::SocketAddr};
+
+async fn read_username(socket: &mut TcpStream) -> Result<String, std::io::Error> {
+    let mut reader = BufReader::new(socket);
+    let mut username = String::new();
+    reader.read_line(&mut username).await?;
+    Ok(username.trim().to_owned())
+}
 
 #[tokio::main]
 async fn main() {
     let listener = TcpListener::bind("0.0.0.0:8080").await.unwrap();
     let (tx, _rx) = broadcast::channel(100);
 
+    let mut users = HashMap::<String, SocketAddr>::new();
+
     loop {
         let (mut socket, addr) = listener.accept().await.unwrap();
         let tx = tx.clone();
         let mut rx = tx.subscribe();
+
+        // Inside the loop, after accepting the connection
+        let username = match read_username(&mut socket).await {
+            Ok(username) => username,
+            Err(e) => {
+                eprintln!("Error reading username: {}", e);
+                continue;
+            }
+        };
+
+        if users.contains_key(&username) {
+            // You can choose how to handle duplicate usernames (e.g., disconnect or generate a unique username)
+            eprintln!("Username '{}' is already in use.", username);
+            continue;
+        }
+
+        let user_addr = addr;
+        users.insert(username.clone(), user_addr);
+
+        // In the spawned task, after the main loop
+        users.remove(&username);
 
         tokio::spawn(async move {
             let (reader, mut writer) = socket.split();
