ffc77cf2ae61e0dd368b46dcafac1ecb8f0d826a
robertkottelin <robert.kottelin@gmail.com>
2023-03-30 21:40:55 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -6,19 +6,16 @@ use tokio::{
 
 #[tokio::main]
 async fn main() {
-    let listener = TcpListener::bind("localhost:8080").await.unwrap();
+    let listener = TcpListener::bind("0.0.0.0:8080").await.unwrap();
     let (tx, _rx) = broadcast::channel(100);
 
     loop {
         let (mut socket, addr) = listener.accept().await.unwrap();
-
-        
         let tx = tx.clone();
         let mut rx = tx.subscribe();
 
         tokio::spawn(async move {
             let (reader, mut writer) = socket.split();
-
             let mut reader = BufReader::new(reader);
             let mut line = String::new();
 
@@ -29,12 +26,14 @@ async fn main() {
                             break;
                         }
 
+                        // Send the line read from the client to all subscribers
                         tx.send((line.clone(), addr)).unwrap();
                         line.clear();
                     }
                     result = rx.recv() => {
                         let (msg, other_addr) = result.unwrap();
 
+                        // Write the message to the client if it's from a different address
                         if addr != other_addr {
                             writer.write_all(msg.as_bytes()).await.unwrap();
                             println!("{}", msg)
