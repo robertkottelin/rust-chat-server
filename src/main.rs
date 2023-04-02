ac099bae067675e901bac261d2fb5fc574eceaff
robertkottelin <robert.kottelin@gmail.com>
2023-04-02 13:58:05 +0200

    user auth implemented

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,17 @@
+use sqlite;
 use std::collections::HashMap;
+use std::sync::Mutex;
 use tokio::{
     io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
     net::TcpListener,
     sync::broadcast,
 };
-use sqlite;
-use std::sync::Mutex;
 
 lazy_static::lazy_static! {
     static ref DB_MUTEX: Mutex<()> = Mutex::new(());
 }
 
 pub fn database(username: &str, password: &str) {
-    println!("Writing to database");
-
     let _lock = DB_MUTEX.lock().unwrap(); // acquire the lock
 
     let connection = sqlite::open("users.db").unwrap();
@@ -22,6 +20,27 @@ pub fn database(username: &str, password: &str) {
         username, password
     );
     connection.execute(&query).unwrap();
+}
+
+pub fn user_auth(username: &str, password: &str) -> bool {
+    let _lock = DB_MUTEX.lock().unwrap(); // acquire the lock
+
+    let connection = sqlite::open("users.db").unwrap();
+    let query = format!(
+        "SELECT COUNT(*) FROM users WHERE username = '{}' AND password = '{}';",
+        username, password
+    );
+
+    let mut result = 0;
+    connection
+        .iterate(&query, |row| {
+            let count_str = row[0].1.unwrap();
+            result = count_str.parse::<i64>().unwrap() as usize;
+            true
+        })
+        .unwrap();
+
+    result > 0
 }
 
 #[tokio::main]
@@ -47,6 +66,7 @@ async fn main() {
                 .unwrap();
 
             // Prompt for username and password
+
             writer.write_all(b"Username: ").await.unwrap();
             let mut username = String::new();
             reader.read_line(&mut username).await.unwrap();
@@ -57,12 +77,24 @@ async fn main() {
             reader.read_line(&mut password).await.unwrap();
             let password = password.trim().to_string();
 
-            // Store the username and password in the hashmap
-            users.insert(username.clone(), password.clone());
-            database(&username, &password); // insert into database
+            let mut authenticated = false;
+            while !authenticated {
+                // Check if the user is authenticated
+                if user_auth(&username, &password) {
+                    authenticated = true;
+                } else {
+                    // If the user is not authenticated, store their credentials and add them to the database
+                    users.insert(username.clone(), password.clone());
+                    database(&username, &password);
+                    writer
+                        .write_all(b"Username-password combination not found. Creating user.\n")
+                        .await
+                        .unwrap();
+                }
+            }
 
             writer
-                .write_all(b"Log in successful... \n")
+                .write_all(format!("Log in successful... Welcome {}! \n", username).as_bytes())
                 .await
                 .unwrap();
 
